c03513f01ad0e5548cb822d4c8e07d6e030421d7
Meikehardianti <meikehardianti25@gmail.com>
2020-10-14 23:32:57 +0700

    Tugas 4

---
+++ Tugas 4/index.php
@@ -0,0 +1,181 @@
+<html>
+<head>
+	<title>CV MEIKE</title>
+	<style type="text/css">
+	</style>
+	<link rel="stylesheet" type="text/css" href="960.css">
+	<link rel="stylesheet" type="text/css" href="Bismillah.css">
+</head>
+<body>
+	<?php 
+	$text1 = "MEIKE HARDIANTI";
+	$text2 = "INFORMATIC'18 ";
+	$text3 = "UPN \"VETERAN\" JAWA TIMUR ";
+	$text4 = "PROFILE";
+	$text4_1 = "Hi! My name is Meike Hardianti, you can call me Meike, I was born in Jombang, Indonesia on 25 May 2020";
+	$text5 = "CONTACT INFORMATION";
+	$text5_1 = "+6285730135991";
+	$text5_2 = "meikehardianti25@gmail.com";
+	$text5_3 = "Sumberaji Kabuh Jombang";
+	$text6 = "SOCIAL MEDIA";
+	$text6_1 = "Meike Hardianti";
+	$text6_2 = "meikehardianti";
+	$text6_3 = "Meike25";
+	$text7 = "EDUCATION";
+	$text7_1 = "(2006-2012)";
+	$text7_2 = "SDN SUMBERAJI 1";
+	$text7_3 = "Elementary School";
+	$text7_4 = "(2012-2015)";
+	$text7_5 = "SMPN 1 KABUH";
+	$text7_6 = "Junior High School";
+	$text7_7 = "(2015-2018)";
+	$text7_8 = "SMAN KABUH";
+	$text7_9 = "Senior High School";
+	$text7_10 = "(2018-now)";
+	$text7_11 = "INFORMATIC";
+	$text7_12 = "UPN \"Veteran\" Jawa Timur";
+	$text8 = "ORGANIZATION";
+	$text8_1 = "2017";
+	$text8_2 = "OSIS SMAN KABUH";
+	$text8_3 = "Senior High School";
+	$text8_4 = "2019";
+	$text8_5 = "Member of Informatic Learning Community G-14";
+	$text8_6 = "Informatic UPN \"Veteran\" Jawa Timur";
+	$text8_7 = "2019";
+	$text8_8 = "Member of UKM Badminton";
+	$text8_9 = "UPN \"Veteran\"  Jawa Timur";
+	$text8_10 = "2020";
+	$text8_11 = "Member of UKM UKKI";
+	$text8_12 = "UPN \"Veteran\" Jawa Timur";
+	$text9 = "INDIVIDUAL";
+	$text9_1 = "Creative";
+	$text9_2 = "Teamwork";
+	$text9_3 = "Communication";
+	$text9_4 = "Intuisi";
+	$text10 = "INTERESTS";
+	$text10_1 = "Listening Music";
+	$text10_2 = "Watching Movie";
+	$text10_3 = "Eating";
+	$text10_4 = "Ice Cream";
+	$text10_5 = "Travelling";
+	$text11 = "LANGUAGE";
+	$text11_1 = "Indonesia";
+	$text11_2 = "English";
+?>
+	<div class="container_12">
+		<div class="grid_5">
+			<div class="body">
+				<img class="foto1" src="foto.jpg">
+				<div class="text1">
+					<?php echo "$text1"; ?> <br>
+					<?php echo "$text2"; ?> <br>
+					<?php echo "$text3"; ?>
+				</div>
+			</div>
+		</div>
+		<div class="grid_7">
+			<h2><?php echo "$text7"; ?></h2>
+			<hr color="#FAC7C7">
+				<table class="text2">
+				<tr>
+					<td><?php echo "$text7_1"; ?></td>
+					<td><?php echo "$text7_2"; ?> <br><?php echo "$text7_3"; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo "$text7_4"; ?></td>
+					<td><?php echo "$text7_5"; ?> <br><?php echo "$text7_6"; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo "$text7_7"; ?></td>
+					<td><?php echo "$text7_8"; ?> <br><?php echo "$text7_9"; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo "$text7_10"; ?></td>
+					<td><?php echo "$text7_11"; ?> <br><?php echo "$text7_12"; ?></td>
+				</tr>
+			</table> <br>
+
+			<h2><?php echo "$text8"; ?></h2>
+			<hr color="#FAC7C7">
+			<table class="text3">
+				<tr>
+					<td><?php echo "$text8_1"; ?></td>
+					<td><?php echo "$text8_2"; ?><br><?php echo "$text8_3"; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo "$text8_4"; ?></td>
+					<td><?php echo "$text8_5"; ?><br><?php echo "$text8_6"; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo "$text8_7"; ?></td>
+					<td><?php echo "$text8_8"; ?><br><?php echo "$text8_9"; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo "$text8_10"; ?></td>
+					<td><?php echo "$text8_11"; ?><br><?php echo "$text8_12"; ?></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+	<div class="container_12">
+		<div class="grid_5">
+			<div class="body2">
+				<div class="text4">
+					<h3><?php echo "$text4"; ?></h3>
+			<hr color="#FAC7C7">
+			<p><?php echo "$text4_1"; ?></p>
+			</div>
+
+			<div class="text5">
+				<h3><?php echo "$text5"; ?></h3>
+			<hr color="#FAC7C7">
+			<img class="foto2" src="telepon.PNG">&emsp;&emsp;&emsp;<?php echo "$text5_1"; ?> <br> <br>
+			<img class="foto2" src="gmail.JPG">&emsp;&emsp;&emsp;<?php echo "$text5_2"; ?> <br> <br>
+			<img class="foto2" src="rumah.PNG">&emsp;&emsp;&emsp; <?php echo "$text5_3"; ?>
+			</div>
+			
+			<div class="text5">
+				<h3><?php echo "$text6"; ?></h3>
+			<hr color="#FAC7C7">
+			<img class="foto2" src="fb.PNG">&emsp;&emsp;&emsp; <?php echo "$text6_1"; ?> <br> <br>
+			<img class="foto2" src="instagram.JPG">&emsp;&emsp;&emsp;<?php echo "$text6_2"; ?><br> <br>
+			<img class="foto2" src="line.JPG">&emsp;&emsp;&emsp; <?php echo "$text6_3"; ?>
+			</div>
+		</div>
+			</div>
+		<div class="grid_7">
+			<h2><?php echo "$text9"; ?></h2>
+			<hr color="#FAC7C7">
+			<h3><?php echo "$text9_1"; ?></h3>
+			<div class="bar1"><div class="bar2"><p>100%</p></div></div>
+			<h3><?php echo "$text9_2"; ?></h3>
+			<div class="bar1"><div class="bar3"><p>100%</p></div></div>
+			<h3><?php echo "$text9_3"; ?></h3>
+			<div class="bar1"><div class="bar4"><p>100%</p></div></div>
+			<h3><?php echo "$text9_4"; ?></h3>
+			<div class="bar1"><div class="bar5"><p>100%</p></div></div>
+
+			<h2><?php echo "$text10"; ?></h2>
+			<hr color="#FAC7C7">
+			&emsp;<img class="foto3" src="musik.JPG">&emsp;&emsp;
+			<img class="foto3" src="tv.PNG">&emsp;
+			<img class="foto3" src="makanan.PNG">&emsp;
+			<img class="foto3" src="eskrim.JPG">&emsp;&emsp;
+			<img class="foto3" src="travelling.JPG"> <br>
+			<b><?php echo "$text10_1"; ?>&emsp;
+			<?php echo "$text10_2"; ?>&emsp;
+			<?php echo "$text10_3"; ?>&emsp;&emsp;
+			<?php echo "$text10_4"; ?>&emsp;&emsp;
+			<?php echo "$text10_5"; ?></b>
+
+			<h2><?php echo "$text11"; ?></h2>
+			<hr color="#FAC7C7">
+			<img class="foto4" src="indonesia.JPG"> &emsp;&emsp;&emsp;
+			<img class="foto4" src="inggris.JPG"> <br>
+			<b>&emsp;<?php echo "$text11_1"; ?> &emsp;&emsp;&emsp;&emsp;&emsp;
+			<?php echo "$text11_2"; ?></b>
+		</div>
+	</div>
+</body>
+</html>
